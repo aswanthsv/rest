428c9e5be1971656b6ae14328228651cb7ddbd7b
aswanthsv <awanthsv@gmail.com>
2026-04-01 19:32:26 +0530

    Added employee GET AdPI

--- EmployeeManagementAPI/Api/EmployeeManagementAPI/EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
+++ EmployeeManagementAPI/Api/EmployeeManagementAPI/EmployeeManagementAPI/obj/Debug/net8.0/EmployeeManagementAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3566173a22ee697bfe9b52f2bd28c3553d21c03")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagementAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
